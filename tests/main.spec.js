4256a52fe89903d8a7cad47bbc67136d7b4ded7a
codefactor-io <support@codefactor.io>
2018-12-10 18:29:35 +0000

    [CodeFactor] Apply fixes

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,40 +1,39 @@
-describe('Main', function() {
-  var arr; // declara a variavel fora do bloco ou scopo
+describe('Main', () => {
+  let arr; // declara a variavel fora do bloco ou scopo
 
   // roda uma vez, antes do bloco
-  before(function() {
+  before(() => {
     // inicia uma conexão no banco
     // criar um conjunto de dados
   });
 
   // roda uma vez, depois do bloco
-  after(function() {
+  after(() => {
     // fecha conexão do banco
     // apagar esse conjunto de dados
   });
 
   // roda todas as vezes, antes de CADA bloco
-  beforeEach(function() {
+  beforeEach(() => {
     arr = [1, 2, 3]; // Atribui a variavel dentro de bloco/scopo ou hooks (no caso de teste)
   });
 
   // roda todas as vezes, depois de CADA bloco
-  afterEach(function() {
+  afterEach(() => {
 
   });
 
-  it('should have a size of 4 when push another value to the array', function() {
+  it('should have a size of 4 when push another value to the array', () => {
     arr.push(4);
     console.log(arr.length); // 4
   });
 
-  it('should remove the value 3 when use pop in the array', function() {
+  it('should remove the value 3 when use pop in the array', () => {
     console.log(arr.pop() === 3); // true
   });
 
-  it('should have a size of 2 when pop a value from the array', function() {
+  it('should have a size of 2 when pop a value from the array', () => {
     arr.pop();
     console.log(arr.length); // 2
   });
-
 });
