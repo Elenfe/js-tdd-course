78146e2e6a832f9fc0e1a92452fa278874ca3c97
elenfe <elenfe7@gmail.com>
2019-01-02 22:02:26 -0200

    teste

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,40 +1,18 @@
-describe('Main', function() {
-  var arr; // declara a variavel fora do bloco ou scopo
+const expect = require('chai').expect;
+const calc = require('../src/main.js');
 
-  // roda uma vez, antes do bloco
-  before(function() {
-    // inicia uma conexão no banco
-    // criar um conjunto de dados
-  });
+describe('calc', () => {
 
-  // roda uma vez, depois do bloco
-  after(function() {
-    // fecha conexão do banco
-    // apagar esse conjunto de dados
-  });
+  // smake tests
+  describe('smoke test', () => {
 
-  // roda todas as vezes, antes de CADA bloco
-  beforeEach(function() {
-    arr = [1, 2, 3]; // Atribui a variavel dentro de bloco/scopo ou hooks (no caso de teste)
-  });
+    it('should exist the calc lib', () => {
+      expect(calc).to.exist;
+    });
 
-  // roda todas as vezes, depois de CADA bloco
-  afterEach(function() {
+    it('should exist the method sum',() => {
+
+    });
 
   });
-
-  it('should have a size of 4 when push another value to the array', function() {
-    arr.push(4);
-    console.log(arr.length); // 4
-  });
-
-  it('should remove the value 3 when use pop in the array', function() {
-    console.log(arr.pop() === 3); // true
-  });
-
-  it('should have a size of 2 when pop a value from the array', function() {
-    arr.pop();
-    console.log(arr.length); // 2
-  });
-
 });
